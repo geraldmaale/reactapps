17589daafd411aa9f27895ba15316146cc2881ac
Gerald Maale <geraldmaale@hotmail.com>
2024-05-05 06:17:56 +0800

    Added shadcn to react-jobs project

--- react-jobsapp/src/components/JobListings.tsx
+++ react-jobsapp/src/components/JobListings.tsx
@@ -1,51 +1,60 @@
 import { useState, useEffect } from "react";
-import JobData from "../data/jobs.json"
+import JobData from "../data/jobs.json";
 import { Job } from "../types/Job";
-import JobCard from "./JobCard"
+import JobCard from "./JobCard";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const JobListings = ({ isHome = false }) => {
-    const [jobs, setJobs] = useState<Job[]>([]);
-    const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [loading, setLoading] = useState(true);
+  const useApi = true;
 
-    useEffect(() => {
-        const jobListings = () => {
-            try {
-                // wait for 1 second to simulate API call
-                setTimeout(() => {
-                    const data = isHome ? JobData.jobs.slice(0, 3) : JobData.jobs;
-                    setJobs(data);
-                }, 1000);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
+  useEffect(() => {
+    const jobListings = async () => {
+      try {
+        if (useApi) {
+          let jobs = await fetch("/api/jobs").then(
+            (res) => res.json() as Promise<Job[]>
+          );
+
+          setTimeout(() => {
+            jobs = isHome ? jobs.slice(0, 3) : jobs;
+            setJobs(jobs);
+          }, 1000);
+        } else {
+          const data = isHome ? JobData.jobs.slice(0, 3) : JobData.jobs;
+          setJobs(data);
+          return;
         }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        jobListings();
-    }, [isHome]);
+    jobListings();
+  }, [isHome, useApi]);
 
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? "Featured Jobs" : "Browse Jobs"}
+        </h2>
 
-    return (
-        <section className="bg-blue-50 px-4 py-10">
-            <div className="container-xl lg:container m-auto">
-                <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    {isHome ? "Featured Jobs" : "Browse Jobs"}
-                </h2>
+        {loading ? (
+          <Skeleton className="w-[100px] h-[20px] rounded-full" />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobCard key={job.id} job={job} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
 
-                {loading
-                    ? (<h1 className="text-4xl">Loading...</h1>)
-                    : (
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                            {jobs.map((job) => (
-                                <JobCard key={job.id} job={job} />
-                            ))}
-                        </div>
-                    )}
-
-            </div>
-        </section>
-    )
-}
-
-export default JobListings
+export default JobListings;
--- react-jobsapp/vite.config.ts
+++ react-jobsapp/vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3100,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL,
+    server: {
+      port: 3100,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api"),
+        },
       },
     },
-  },
+    resolve: {
+      // Resolve shadcn alias
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
